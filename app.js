5026634faff171e7876b75b717ffae9b0b03dc8e
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-04 10:26:42 +0530

    Establish mongo connection to atlas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphQlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
@@ -62,4 +63,13 @@ app.use(
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-tjanz.mongodb.net/test?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(error => {
+    console.log(error);
+  });
