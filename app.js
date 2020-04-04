25e4952456b3e444d1bb37ca70ab652795e1cb68
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-04 10:35:17 +0530

    Add events model

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use(
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-tjanz.mongodb.net/test?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-tjanz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(3000);
