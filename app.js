d7f4f5c9d9eef8d04de375ff6128735770abf1d9
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-04 11:02:44 +0530

    Update cloud atlase via graphql mutate

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const bodyParser = require("body-parser");
 const graphQlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+
+const Event = require("./models/event");
+
 const app = express();
 
 app.use(bodyParser.json());
-
-const events = [];
 
 app.use(
   "/graphql",
@@ -45,18 +46,33 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc };
+            });
+          })
+          .catch(error => {
+            throw error;
+          });
       },
       createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(response => {
+            console.log(response);
+            return { ...response._doc };
+          })
+          .catch(error => {
+            console.log(error);
+            throw error;
+          });
       }
     },
     graphiql: true
@@ -65,7 +81,7 @@ app.use(
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-tjanz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-tjanz.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(3000);
