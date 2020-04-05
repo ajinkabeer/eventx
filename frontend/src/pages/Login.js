f842f6abccccc5905122e5dfa12f741b538cb241
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-06 00:51:27 +0530

    Hit api with credentials

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,24 +1,69 @@
 import React, { Component } from "react";
 import "./Login.css";
-class Auth extends Component {
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+
+  submitHandler = async (event) => {
+    event.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    const query = {
+      query: `
+        mutation {
+          createUser(userInput:{email:"${email}",password:"${password}"}){
+            _id
+            email
+          }
+        }
+      `,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/graphql", {
+        method: "POST",
+        body: JSON.stringify(query),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.status !== 200 && response.status !== 201) {
+        throw new Error("Failed");
+      }
+      const responseData = await response.json();
+      console.log(responseData);
+    } catch (error) {
+      throw error;
+    }
+  };
+
   render() {
     return (
-      <form class="login-form">
+      <form className="login-form" onSubmit={this.submitHandler}>
         <div className="form-control">
-          <label for="email">Email</label>
-          <input type="email" id="email" />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" ref={this.emailEl} />
         </div>
         <div className="form-control">
-          <label for="password">Password</label>
-          <input type="password" id="password" />
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
         </div>
         <div className="form-actions">
-          <button type="submit">Login</button>
-          <button type="button">SignUp</button>
+          <button type="submit">SignUp</button>
+          <button type="button">Login</button>
         </div>
       </form>
     );
   }
 }
 
-export default Auth;
+export default Login;
