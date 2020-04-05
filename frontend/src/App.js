9b47f80d0ca71e85aa61fc8514518e2e88cd33ee
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-05 23:43:35 +0530

    Add basic pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
-import AuthPage from "./pages/Auth";
+import Login from "./pages/Login";
+import Bookings from "./pages/Bookings";
+import Events from "./pages/Events";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Redirect from="/" to="/login" exact />
-        <Route path="/login" component={AuthPage} />
-        <Route path="/events" component={null} />
-        <Route path="/bookings" component={null} />
+        <Route path="/login" component={Login} />
+        <Route path="/events" component={Events} />
+        <Route path="/bookings" component={Bookings} />
       </Switch>
     </BrowserRouter>
   );
