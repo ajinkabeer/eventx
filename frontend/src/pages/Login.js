017ac3a3319fb81c660a2deadf2ea350b4162cb3
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-06 00:23:42 +0530

    Update login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
-
+import "./Login.css";
 class Auth extends Component {
   render() {
-    return <h1>Login Page</h1>;
+    return (
+      <form class="login-form">
+        <div className="form-control">
+          <label for="email">Email</label>
+          <input type="email" id="email" />
+        </div>
+        <div className="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Login</button>
+          <button type="button">SignUp</button>
+        </div>
+      </form>
+    );
   }
 }
 
