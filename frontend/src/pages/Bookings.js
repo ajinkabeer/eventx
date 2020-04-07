0830be76a4a32070aaea18558ee5f4bd0edfeade
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-07 23:53:40 +0530

    Add cancel booking function

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import LoginContext from "../context/login";
 import Spinner from "../components/Spinner/Spinner";
+import BookingList from "../components/Bookings/BookingsList/BookList";
 
 class Bookings extends Component {
   state = {
@@ -52,19 +53,53 @@ class Bookings extends Component {
     }
   };
 
+  deleteBookingHandler = async (bookingId) => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
+          mutation {
+            cancelBooking(bookingId:"${bookingId}"){
+              _id
+              title
+            }
+          }
+        `,
+    };
+    try {
+      const response = await fetch("http://localhost:3000/graphql", {
+        method: "POST",
+        body: JSON.stringify(requestBody),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + this.context.token,
+        },
+      });
+      if (response.status !== 200 && response.status !== 201) {
+        throw new Error("Failed");
+      }
+      await response.json();
+      this.setState((prevState) => {
+        const updatedBookings = prevState.bookings.filter((booking) => {
+          return booking._id !== bookingId;
+        });
+        return { bookings: updatedBookings, isLoading: false };
+      });
+    } catch (error) {
+      this.setState({ isLoading: false });
+      throw error;
+    }
+  };
+
   render() {
     return (
       <>
         {this.state.isLoading ? (
           <Spinner />
         ) : (
-          <ul>
-            {this.state.bookings
-              ? this.state.bookings.map((booking) => (
-                  <li key={booking._id}>{booking.event.title}</li>
-                ))
-              : null}
-          </ul>
+          <BookingList
+            bookings={this.state.bookings}
+            onDelete={this.deleteBookingHandler}
+          />
         )}
       </>
     );
