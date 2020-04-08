77deaf2f71eed7abe4aec999dd3da6ad5490354e
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-04-08 12:43:02 +0530

    Ditch refs for controlled components in login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -6,15 +6,13 @@ import "./css/login.css";
 class Login extends Component {
   state = {
     isLogin: true,
+    emailInvalid: false,
+    passwordInvalid: false,
+    email: "",
+    password: "",
   };
 
   static contextType = LoginContext;
-
-  constructor(props) {
-    super(props);
-    this.emailEl = React.createRef();
-    this.passwordEl = React.createRef();
-  }
 
   switchModeHandler = () => {
     this.setState((prevState) => {
@@ -23,12 +21,11 @@ class Login extends Component {
   };
 
   submitHandler = async (event) => {
+    const { email, password } = this.state;
     event.preventDefault();
-    const email = this.emailEl.current.value;
-    const password = this.passwordEl.current.value;
 
     if (email.trim().length === 0 || password.trim().length === 0) {
-      return;
+      this.setState({ emailInvalid: true, passwordInvalid: true });
     }
 
     let requestBody = {
@@ -73,10 +70,12 @@ class Login extends Component {
         },
       });
       if (response.status !== 200 && response.status !== 201) {
-        throw new Error("Failed");
+        if (email.trim().length !== 0 || password.trim().length !== 0) {
+          toast.info("User exists");
+        }
       }
       const responseData = await response.json();
-      if (!this.state.isLogin) {
+      if (!this.state.isLogin && !responseData.errors) {
         toast.success("User created 🚀");
       }
       if (responseData.data.login !== undefined) {
@@ -89,7 +88,23 @@ class Login extends Component {
         localStorage.setItem("userId", responseData.data.login.userId);
       }
     } catch (error) {
-      throw error;
+      return;
+    }
+  };
+
+  emailChangeHandler = (e) => {
+    const email = e.target.value;
+    this.setState({ email });
+    if (email.trim().length === 0) {
+      this.setState({ emailInvalid: true });
+    }
+  };
+
+  passwordChangeHandler = (e) => {
+    const password = e.target.value;
+    this.setState({ password });
+    if (password.trim().length === 0) {
+      this.setState({ passwordInvalid: true });
     }
   };
 
@@ -100,16 +115,37 @@ class Login extends Component {
 
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" ref={this.emailEl} autoComplete="on" />
+          <input
+            className={this.state.emailInvalid ? "invalid" : ""}
+            type="email"
+            id="email"
+            autoComplete="on"
+            value={this.state.email}
+            onChange={(e) => this.emailChangeHandler(e)}
+          />
+          <label
+            className={this.state.emailInvalid ? "error" : "no-error"}
+            htmlFor="error"
+          >
+            Invalid e-mail
+          </label>
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
           <input
+            className={this.state.passwordInvalid ? "invalid" : ""}
             type="password"
             id="password"
-            ref={this.passwordEl}
             autoComplete="on"
+            value={this.state.password}
+            onChange={(e) => this.passwordChangeHandler(e)}
           />
+          <label
+            className={this.state.passwordInvalid ? "error" : "no-error"}
+            htmlFor="error"
+          >
+            Invalid Email
+          </label>
         </div>
         <div className="form-actions">
           <button type="submit">Submit</button>
